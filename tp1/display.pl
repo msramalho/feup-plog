3023252414d645e40ceb8bb68a63d0edfb166ceb
msramalho <up201403027@fe.up.pt>
2017-10-11 12:02:59 +0100

    [display] the file to process the display of the board

---
+++ tp1/display.pl
@@ -0,0 +1,50 @@
+/* This file implements the predicates required for displaying the board */
+
+/*
+Color  code     countInGame    countOffGame
+Black:  D           8               1
+Red:    R           8               1
+Ivory:  I           8               1
+Green:  G           8               1
+Blue:   B           8               1
+Wild:   W           3               0
+
+
+Examples: delete afterwards
+    0    1     2     3     4     5     6     7     8
+ 0                |     |     |     |     |
+ 1                |     |     |     |     |
+ 2                |     |     |     |     |     |
+ 3          |     |     |     |     |     |     |
+ 4          |     |     |     |     |     |     |
+ 5          |     |     |     |     |     |     |
+ 6    |     |     |     |     |     |     |     |     |
+ 7          |     |     |     |     |     |     |
+ 8          |     |     |     |     |     |     |
+ 9          |     |     |     |     |     |     |
+10                |     |     |     |     |
+11                |     |     |     |     |
+12                |     |     |     |     |
+-------------------------------------------------------
+Player1 - R,B.
+Player2 - I,G.
+Now playing: Player1.
+
+
+GRB - stack com G em baixo, R por cima de G, B por cima de
+
+    0    1     2     3     4     5     6     7     8
+ 0                |  R  |  G  |  W  | GRB |
+ 1                |     |     |     |     |
+ 2                |     |     |     |     |     |
+ 3          |     |     |     |     |     |     |
+ 4          |     |     |     |     |     |     |
+ 5          |     |     |     |     |     |     |
+ 6    |     |     |     |DRBWI|     |     |     |     |
+ 7          |     |     |     |     |     |     |
+ 8          |     |     |     |     |     |     |
+ 9          |     |     |     |     |     |     |
+10                |     |     |     |     |
+11                |     |     |     |     |
+12                |     |     |     |     |
+*/
