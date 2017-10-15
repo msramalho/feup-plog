631c04d8a8dac9feb324840eb5718db8ab95e18c
msramalho <up201403027@fe.up.pt>
2017-10-15 22:08:27 +0100

    test display predicate

--- tp1/main.pl
+++ tp1/main.pl
@@ -2,3 +2,10 @@
 :- include('display.pl').
 :- include('board.pl').
 :- include('examples.pl').
+
+test:-
+    emptyBoard(B),
+    P1 = [red, green],
+    P2 = [ivory, empty],
+    Playing = player1,
+    displayBoard(B, P1, P2, Playing).
