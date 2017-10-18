63ac2fc1ce85d52e9750098d153e1ec4925269c5
msramalho <up201403027@fe.up.pt>
2017-10-18 12:13:05 +0100

    interface updates, super reusable

--- tp1/interface.pl
+++ tp1/interface.pl
@@ -1,14 +1,20 @@
-wulc:-put_code(9484). % write upper left corner
-wurc:-put_code(9488). % write upper right corner
-wllc:-put_code(9492). % write lower left corner
-wlrc:-put_code(9496). % write lower right corner
-
-wvd:-put_code(9474). % write vertical dash
+wulc:-put_code(9484). % write upper left corner ┌
+wurc:-put_code(9488). % write upper right corner ┐
+wllc:-put_code(9492). % write lower left corner ┘
+wlrc:-put_code(9496). % write lower right corner └
+wlf:-put_code(9500). % write left fork ├
+wrf:-put_code(9508). % write right fork ┤
+wvd:-put_code(9474). % write vertical dash│
 %write dash
 wd:-put_code(9472).
 wd(0).
 wd(N):-wd, N1 is N-1, wd(N1).
 
+% write dashed separator line
+wsl(Len):-wlf, wd(Len), wrf, nl.
+
+
+% write line with content center aligned
 wel(Len):-wel(Len, ' ').
 wel(Len, Content):-
 	Start = '~|~t~s~t~', End = '+',
@@ -18,9 +24,14 @@ wel(Len, Content):-
 displayMenu:-
 	write('\33\[2J'),
 	wulc, wd(40), wurc, nl,
-	wel('30'), wel('30'), wel('30'),
-	wel('30', 'Welcome to'),
-	wel('30', 'LYNGK'),
-	wel('30'), wel('30'),wel('30'),wel('30'),
+	wel('30'),
+	wel('30', 'LYNGK GAME'),
+	wel('30'),
+	wsl(40),
+	wel('30'), wel('30'),wel('30'),
+	wel('30', '1: Start Game'),
+	wel('30', '2: See operations'),
+	wel('30', '3: Quit'),
+	wel('30'), wel('30'),wel('30'),
 	wllc, wd(40), wlrc.
 
