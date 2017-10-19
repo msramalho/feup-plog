b7e59efb5122b06b7f5f629a5da666810a8c7e38
msramalho <up201403027@fe.up.pt>
2017-10-19 16:07:41 +0100

    listToString and numberToString

--- tp1/utils.pl
+++ tp1/utils.pl
@@ -5,3 +5,14 @@ setRandomSeed:-
 	now(Time), S is Time mod 30269,
 	getrand(random(X, Y, Z, _)),
 	setrand(random(S, X, Y, Z)), !.
+
+listToString(List, Result):-
+	listToString(List, '', Result).
+listToString([], Final, Final).
+listToString([H|T], Temp, Final):-
+	atom_concat(Temp, H, NewRes),
+	listToString(T, NewRes, Final).
+
+numberToString(Number, String):-
+	number_chars(Number, TempList),
+	listToString(TempList, String).
