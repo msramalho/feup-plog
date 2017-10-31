c52f73f6054c2297386696a31be59a252167d1ee
msramalho <up201403027@fe.up.pt>
2017-10-31 18:37:41 +0000

    clear function

--- tp1/displayBoard.pl
+++ tp1/displayBoard.pl
@@ -101,7 +101,7 @@ displayPlayerStats(Player):-
 
 %clears the screen and ouputs the board and the game state
 displayBoard:-
-	write('\33\[2J'),
+    clear,
 	write('      0    1    2    3    4    5    6    7    8\n\n'),
     board(Board),
 	displayLine(Board, 0),
--- tp1/utils.pl
+++ tp1/utils.pl
@@ -36,3 +36,7 @@ getBoardTopColor(X, Y, TopColor):-
 %not implementation
 not(X):-X, !, fail.
 not(_).
+
+
+
+clear:-write('\33\[2J').
