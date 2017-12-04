8a9b06fa2dc2552bc7625a57944f12819a32b66f
msramalho <up201403027@fe.up.pt>
2017-12-04 00:46:38 +0000

    [pl] continued restrictions
    The minimization still doesn't work, needs better domain on variables

--- tp2/src/main.pl
+++ tp2/src/main.pl
@@ -1,6 +1,6 @@
 :- use_module(library(lists)).
 :- use_module(library(clpfd)).
-:- use_module(library(system)).
+:- use_module(library(between)).
 
 :- include('data.pl').
 :- include('utils.pl').
@@ -14,25 +14,77 @@ init(Subjects, Teachers):-
 % 1 - variable definition + 2 - domain specification
     defineTeachers(Teachers),
     defineSubjects(Subjects),
+    % PreferenceFailedCount in 0..40,
+    FailedHours in 0..20,
+    write('1\n'),
 
 % 3 - restrictions application
     %Teachers preference on the diference
     restrictTeachers(Teachers),
+    write('2\n'),
     length(Teachers, NTeachers), %get the number of teachers
-    restrictSubjects(Subjects, NTeachers, Teachers),
+    write('3\n'),
+    getMatrixOfComplementedFields(Teachers, CompFields),
+    write('4\n'),
+    restrictSubjects(Subjects, CompFields, NTeachers, PreferenceFailedCount),
+    write(PreferenceFailedCount), nl,
+    write('5\n'),
+    restrictSumBySemester(Subjects, Teachers),
+    write('6\n'),
+    getMatrixOfComplementedFields(Teachers, CompFields),
+    write('7\n'),
+    getFailedHours(Teachers, FailedHours),
+    write(FailedHours), nl,
+    write('8\n'),
 
 % 4 - search for solutions
     getTeachersVariablesToLabel(Teachers, TVars),
     labeling([], TVars).
+    %see: https://www.informatik.uni-kiel.de/~curry/listarchive/0817.html
+    % labeling([minimize(FailedHours)], TVars).
+    % labeling([minimize(PreferenceFailedCount)], TVars).
 
 
-% variable definition helpers
+%------------------------------------variable definition helpers
 defineSubjects(Subjects):-
     findall([Semester-Field-HT-HP-DT-DP, _, _], subject(Semester, HT, HP, DT, DP, Field), Subjects).
 defineTeachers(Teachers):-
     findall(Avg-Diff-Field-_HS1-_HS2, (teacher(Type, Diff, Field), teacherType(Type, Avg)), Teachers).
 
-%restrictions on lists helpers
+%get a list with the TT and TP for all Subjects (of a Semester) in a List
+getSubjectsTimesBySemester([], _, []).
+getSubjectsTimesBySemester([[Semester-_Field-_HT-_HP-_DT-_DP, TT, TP]|R], Semester, NewTimes):-
+    getSubjectsTimesBySemester(R, Semester, Times),
+    append([TT, TP], Times, NewTimes).
+%skip if the semester does not match
+getSubjectsTimesBySemester([_|R], Semester, Times):-getSubjectsTimesBySemester(R, Semester, Times).
+
+%get a matrix with NFields lines, where each line has NTeachers elements, where each cell is 1 or 0 (1 if the corresponding teacher DOES NOT teach this field) - this worked on the first attempt - self-five
+getMatrixOfComplementedFields(Teachers, CompFields):-
+    length(Teachers, NTeachers),
+    fields(NFields), %the number of fields
+    numlist(1, NFields, Fields),%list with all the numbers from 1 to NFields
+    findall(L, (
+        length(L, NTeachers), %set the length for this line
+        member(Field, Fields), %get the field matching this line
+        findall(Complement, (
+            member(_Avg-_Diff-TField-_HS1-_HS2, Teachers), %get the teacher Field
+            Field #\= TField #<=> Complement % get the complement
+        ), L)
+    ), CompFields).
+
+getFailedHours(Teachers, FailedHours):-
+    findall(D, (
+        member(Avg-_Diff-_Field-HS1-HS2, Teachers),
+        Temp #= (HS1 + HS2) / 2,
+        D #= Avg - Temp
+    ),FailedHoursList),
+    write(FailedHoursList),
+    sum(FailedHoursList, #=, FailedHours).
+
+
+
+%------------------------------------restrictions on lists helpers
 restrictTeachers([]).
 restrictTeachers([Avg-Diff-_Field-HS1-HS2|T]):-
     MaxHours is Avg * 2, % the maximum amount of hours for a teacher in a semester = 2 * avg (extreme cases)
@@ -42,8 +94,10 @@ restrictTeachers([Avg-Diff-_Field-HS1-HS2|T]):-
     %recursive call
     restrictTeachers(T).
 
-restrictSubjects([], _, _).
-restrictSubjects([[_Semester-_Field-HT-HP-DT-DP, TT, TP]|R], NTeachers, Teachers):-
+restrictSubjects([], _, _, 0).
+restrictSubjects([[_Semester-Field-HT-HP-DT-DP, TT, TP]|R], CompFields, NTeachers, PreferenceFailedCount):-
+    %recursive call
+    restrictSubjects(R, CompFields, NTeachers, TempCount),
     % number of teachers needed for Theoretical (Total Hours/Division)
     length(TT, NTeachers),
     sum(TT, #=, HT),
@@ -52,43 +106,47 @@ restrictSubjects([[_Semester-_Field-HT-HP-DT-DP, TT, TP]|R], NTeachers, Teachers
     % number of teachers needed for Pratical (Total Hours/Division)
     length(TP, NTeachers),
     sum(TP, #=, HP),
-    generateFdset(0, HT, DP, PFdset),
+    generateFdset(0, HP, DP, PFdset),
     domainFdset(TP, PFdset),
     %restrict the domain of the subjects to the total number of teachers
     domain(TT, 0, HT),
     domain(TP, 0, HP),
+    %restrict to only allow teachers of the field in the Theoretical lessons
+    nth1(Field, CompFields, FieldComplements),
+    scalar_product(FieldComplements, TT, #= , 0), %Restriction-3,
+    scalar_product(FieldComplements, TP, #= , CurrentCount), % minimize this, Restriction-4
+    PreferenceFailedCount #= TempCount + CurrentCount.
 
-    %restrict the number of hours by the teachers
-    % restrictHoursTeachers(HP-DT, HP-DP, Teachers),
 
-    %field restrictions
-    % restrictSubjectField(Field, TT, Teachers)
+%make sure the sum of the times for each subject, in both semesters, match that of the teachers
+restrictSumBySemester(Subjects, Teachers):-
+    %semester 1
+    getSubjectsTimesBySemester(Subjects, 1, MatrixTimesS1),%matrix like [TT1,TP1,TT2,TP2,TT3,...]
+    scalarSumMatrix(MatrixTimesS1, TimesS1),%sum every line in the matrix into TimesS
+    restrictTeacherSemester1(Teachers, TimesS1),%match the teacher's time with the corresponding cell
+    %semester 2
+    getSubjectsTimesBySemester(Subjects, 2, MatrixTimesS2),%matrix like [TT1,TP1,TT2,TP2,TT3,...]
+    scalarSumMatrix(MatrixTimesS2, TimesS2),%sum every line in the matrix into TimesS
+    restrictTeacherSemester2(Teachers, TimesS2).%match the teacher's time with the corresponding cell
 
-    %recursive call
-    restrictSubjects(R, NTeachers, Teachers).
 
-%generate an fdset with a start, end and step
-generateFdset(Min, Max, Step, Fdset):-
-    generateList(Min, Max, Step, List),
-    list_to_fdset(List, Fdset).
-%generate a list with a start, end and step
-generateList(Max, Max, _Step, [Max]).
-generateList(Min, Max, Step, NewList):-
-    NewMin is Min + Step,
-    generateList(NewMin, Max, Step, List),
-    NewList = [Min|List].
+%make sure the sum of hours of a teacher in the first semester matches his/her HS1
+restrictTeacherSemester1(_, []):-!.
+restrictTeacherSemester1([], []).
+restrictTeacherSemester1([_Avg-_Diff-_Field-Sum-_HS2|T1], [Sum|T2]):-restrictTeacherSemester1(T1, T2).
+%make sure the sum of hours of a teacher in the first semester matches his/her HS2
+restrictTeacherSemester2(_, []):-!.
+restrictTeacherSemester2([], []).
+restrictTeacherSemester2([_Avg-_Diff-_Field-_HS1-Sum|T1], [Sum|T2]):-restrictTeacherSemester2(T1, T2).
 
-%apply a domain restriction to every element of a list, namely in_set
-domainFdset([], _).
-domainFdset([N|R], Fdset):-
-    N in_set Fdset,
-    domainFdset(R, Fdset).
 
-%labeling helpers
+%------------------------------------labeling helpers
 getTeachersVariablesToLabel([], []).
 getTeachersVariablesToLabel([_Avg-_Diff-_Field-HS1-HS2|T], TVars):-
     getTeachersVariablesToLabel(T, TempTVars),
     append(TempTVars, [HS1, HS2], TVars).
+
+
 
 /*
 Everything is 1 indexed
@@ -115,9 +173,8 @@ Modeling:
 Restrictions:
     1. A teacher chooses the acceptable Diff value for the difference in hours from the 1st to the 2nd semesters;
     2. According to their type, teachers will need to teach an average week teaching time over the two semesters that is approximate to the stipulated values.
-
-    . Theoretical Hours must be lectured by teachers that have the same field
-    . Pratical lessons should be given by teachers of that area, prefearbly (minimize the number of teachers that teach something they "do not know")
+    3. Theoretical Hours must be lectured by teachers that have the same field
+    4. Pratical lessons should be given by teachers of that area, prefearbly (minimize the number of teachers that teach something they "do not know")
 
     . maximize the number of teachers with their preferences respected
 
