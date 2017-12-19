e66ff5f28f76c4b30b717945fb0873d65dee2712
msramalho <up201403027@fe.up.pt>
2017-12-19 23:57:00 +0000

    tester improved + interface almost ready

--- tp2/interface.py
+++ tp2/interface.py
@@ -1,4 +1,5 @@
 import argparse
+import base64
 
 from prologer import *
 from generator import *
@@ -13,7 +14,8 @@ groupActions = parser.add_argument_group('actions')
 groupActions.add_argument('-g','--generate', help='Generate a JSON file (-jf)', default=False, action='store_true')
 groupActions.add_argument('-p','--parse', help='Parse from a JSON file into a Prolog file(-jf to -pf)', default=False, action='store_true')
 groupActions.add_argument('-e','--execute', help='Execute the main.pl file with a custom Prolog data file (-pf, -cf)', default=False, action='store_true')
-groupActions.add_argument('-tt','--test', help='Run the hardcoded tests', default=False, action='store_true')
+groupActions.add_argument('-tt','--test', help='Run all the json files inside data/', default=False, action='store_true')
+groupActions.add_argument('-ht','--hardcoded-test', help='Run the hardcoded tests', default=False, action='store_true')
 
 # generation arguments
 groupGenArgs = parser.add_argument_group('generation arguments')
@@ -24,37 +26,57 @@ groupGenArgs.add_argument('-md','--max-diff', metavar="", help='Max Diff value f
 # custom filenames
 groupFiles = parser.add_argument_group('custom filenames')
 groupFiles.add_argument('-jf','--json-file', metavar="", help='The name of the json file to be used (generation, parsing)', default="auto_gen.json")
-groupFiles.add_argument('-pf','--prolog-file', metavar="", help='The name of the prolog file to be used (parsing, execution)', default="data.pl")
+groupFiles.add_argument('-pf','--prolog-file', metavar="", help='The name of the prolog file to be used (parsing, execution)', default="src/data.pl")
 groupFiles.add_argument('-cf','--csv-file', metavar="", help='The name of the csv file to save the results (not specified = no output)', default=False)
 
 # behaviour customization
 groupCustom = parser.add_argument_group('behaviour customization')
-groupCustom.add_argument('-t','--tabled', help='Makes the output form JSON to Prolog be tabled for easy reading', default=False, action='store_true')
-groupCustom.add_argument('-r','--randomize', help='randomize the number of effective hours, instead of using all available (for the generator)', default=False, action='store_true')
+groupCustom.add_argument('-r','--remove', help='Remove the generated files', default=False, action='store_true')
 groupCustom.add_argument('-d','--debug', help='Run the prolog code in debug mode (only for executing)', default=False, action='store_true')
+groupCustom.add_argument('-t','--tabled', help='Makes the output form JSON to Prolog be tabled for easy reading', default=False, action='store_true')
+groupCustom.add_argument('-re','--randomize', help='randomize the number of effective hours, instead of using all available (for the generator)', default=False, action='store_true')
 
 # parse the arguments
 args = vars(parser.parse_args())
-args["prolog_file"] = "src/" + args["prolog_file"]
 
 #---------------------------------------Logic
 subjects, teachers = [], []
+generatedFilesToRemove = []
 # generate
 if args["generate"]:
 	config = Config(rounds=args["number_rounds"], maxDiff=args["max_diff"], nFields=args["number_fields"], randomizeEfficient=args["randomize"])
 	subjects, teachers = generate(config)
 	dataToPrologJson(config, subjects, teachers, args["json_file"])
+	generatedFilesToRemove.append(args["json_file"])
 
 # parse
 if args["parse"]:
 	generatePrologForFile(args["json_file"], tabled=args["tabled"], output=args["prolog_file"])
+	generatedFilesToRemove.append(args["prolog_file"])
 
 # execute
 if args["execute"]:
 	filesToRemove = []
-	newMain = createNewMainFile(args["json_file"], filesToRemove)
-	print(newMain)
-	output, diff = executeMainFile(newMain)
+	newMain = createNewMainFile(args["prolog_file"], filesToRemove)
+	output, diff = executeMainFile(newMain, args["debug"])
 	removeTmpFiles(filesToRemove)
-	print(output)
+	if args["csv_file"]:
+		csvHeader(args["csv_file"])
+		outputToCsv(args["csv_file"], (output, None))
+
+# remove generated files
+if args["remove"]:
+	removeTmpFiles(generatedFilesToRemove)
+
+if __name__ == "__main__": # required for the multiprocessing
+	# all tests
+	if args["test"]:
+		runAll(args["csv_file"])
+
+	# hardcoded tests
+	if args["hardcoded_test"]:
+		# runAll(args["csv_file"])
+		# repetitions, nrounds, maxDiff, nFields
+		pass
+
 
--- tp2/tester.py
+++ tp2/tester.py
@@ -1,52 +1,51 @@
 import subprocess as sp  # call, Popen, PIPE
 from os import listdir, remove, mkdir
 import os
-from os.path import isfile, join
+from os.path import isfile, join, basename
 from multiprocessing import Manager, Pool, cpu_count
 import time
-import ntpath
 from sys import argv, exit
+import re
+from statistics import mean
 
 from prologer import *
 from generator import *
 
 #important variables:
 tmpFolder = "src/"#folder to use for the temporary usage of files, should not be inner folder, due to include referencing
-dataPrefix = "data_"#append to generated data prolog files
 mainPrefix = "main_"#append to generated data prolog files
 dataFolder = "data/"#source folder to look for json files
+outputVariables = ["Teachers: ", "Subjects: ", "Fields: ", "Heuristic: ", "Failed Hours: ", "#Practical Teacher From Other Field: ", "Resumptions: ", "Entailments: ", "Prunings: ", "Backtracks: ", "Constraints created: ", "Walltime: "]
 
 def createPrologDataFromJson(jsonFile, filesToRemove):
 	outputFileName = jsonFile[:-4] + "pl" # mieic_a3_s1.pl
-	completeOutputFileName = tmpFolder + dataPrefix + outputFileName # src/mieic_a3_s1.pl
+	completeOutputFileName = tmpFolder + outputFileName # src/mieic_a3_s1.pl
 	completeJsonFile = dataFolder + jsonFile
 	generatePrologForFile(completeJsonFile, output=completeOutputFileName) #generate the .pl file from the json
 	filesToRemove.append(completeOutputFileName)#add to the list of files to remove
+	return outputFileName
 
-def createNewMainFile(jsonFile, filesToRemove, edit="src/main.pl"):
+def createNewMainFile(plFile, filesToRemove, edit="src/main.pl"):
+	plFile = basename(plFile)
 	contents = ""
-	jsonToPl = jsonFile[:-4]+"pl"
 	with open(edit, 'r', encoding="utf-8") as mainFile:#read default main file
 		contents = mainFile.read()
-	contents = contents.replace("data.pl", dataPrefix + jsonToPl)#replace included data file
-	newMainFilename = getValidFileName(tmpFolder + mainPrefix + jsonToPl)
+	contents = contents.replace("data.pl", plFile)#replace included data file
+	newMainFilename = getValidFileName(tmpFolder + mainPrefix + plFile)
 	writeFileWarnDuplicate(newMainFilename, contents)# write to file
 	filesToRemove.append(newMainFilename)#add to the list of files to remove
 	return newMainFilename
 
-def executeMainFile(newMain):
+def executeMainFile(newMain, debug = True):
+	debug = "true" if debug else "false"
 	start = time.time()#time measurement start
 
-	cmd = "echo init(Subjects, Teachers). | sicstus --nologo --noinfo -l %s" % newMain
+	cmd = "echo init(Subjects, Teachers, %s). | sicstus --nologo --noinfo -l %s" % (debug, newMain)
 	process = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
 	processOutput = processToStdout(process)
 
 	diff = (time.time() - start) * 1000000 # microseconds, time measurement end
 	return (processOutput, diff) # return tuple with output, time in microseconds
-
-def executeMainFileToShell(newMain):
-	cmd = "echo init(Subjects, Teachers). | sicstus --nologo --noinfo -l %s" % newMain
-	sp.call(cmd, shell=True)
 
 #read the output of a subprocess, print and return it
 # it is done this way so that later we can parse the stdout in python
@@ -71,26 +70,68 @@ def removeTmpFiles(filesToRemove):
 if not os.path.exists(tmpFolder):
 	os.makedirs(tmpFolder)
 
-def worker(jsonFile):
+def worker(jsonFileTuple):
+	jsonFile, outputFile = jsonFileTuple
 	filesToRemove = []
-	createPrologDataFromJson(jsonFile, filesToRemove)
-	newMain = createNewMainFile(jsonFile, filesToRemove)
+	plFile = tmpFolder + createPrologDataFromJson(jsonFile, filesToRemove)
+	newMain = createNewMainFile(plFile, filesToRemove)
 	result = executeMainFile(newMain)
+	print(newMain + " - done")
 	removeTmpFiles(filesToRemove)
-	return (jsonFile, result)
+	#write to csv
+	if outputFile:
+		outputToCsv(outputFile, result)
+	return (plFile, result)
 
 def outputResults(results):
 	for (f, (output, time)) in results:
 		print("-" * 50)
 		print("%s lasted for %10f microseconds" % (f, time))
-		result = {}
 		#get the output variables from prolog
-		outputVariables = ["Resumptions: ", "Entailments: ", "Prunings: ", "Backtracks: ", "Constraints created: ", "Runtime: ", "Walltime: "]
+		result = {}
 		for outVar in outputVariables:
 			result[outVar] = [line.strip("\r").strip(outVar) for line in output.split('\n') if outVar in line]
 		print(json.dumps(result))
 
-if __name__ == '__main__':
+#get the csv file headers
+def csvHeader(outputFile):
+	headerTitles = []
+	for outVar in outputVariables:
+		temp = re.sub('[!@#:$]', '', outVar.strip())
+		headerTitles.append(re.sub('[ ]', '_', temp))
+	with open(outputFile, 'w', encoding="utf-8") as f:  # write the new main
+		f.write(", ".join(headerTitles) + "\n")
+
+def csvFromOutput(output):
+	print("output: ", output)
+	res = []
+	for outVar in outputVariables:
+		temp = output[outVar]
+		if type(temp) is list:
+			temp = list(map(lambda x: float(x.strip("%")), temp)) # convert to floats
+			temp = str(mean(temp) if len(temp) > 0 else 0) # convert to mean or to string
+		res.append(temp)
+	return ", ".join(res)
+
+def outputToCsv(outputFile, resTuple):
+	output, _ = resTuple
+	#get the output variables from prolog
+	result = {}
+	for outVar in outputVariables:
+		result[outVar] = [line.strip("\r").strip(outVar) for line in output.split('\n') if outVar in line]
+	with open(outputFile, 'a', encoding="utf-8") as f:  # write the new main
+		f.write(csvFromOutput(result) + "\n")
+
+def runAll(outputFile = None):
+	jsonFilesTuple = [(f, outputFile) for f in listdir(dataFolder) if isfile(join(dataFolder, f)) and f[-5:] == ".json"]
+	print("Found %d file(s)" % (len(jsonFilesTuple)))
+
+	nCores = cpu_count()#number of cpu cores
+	with Pool(nCores) as pool:
+		work_results = pool.map(worker, jsonFilesTuple)
+	outputResults(work_results)
+
+''' if __name__ == '__main__':
 	if len(argv) >= 2:
 		if argv[1] == "gen":#run with a generator
 			rounds = int(argv[2]) if len(argv) >= 3 else 1
@@ -103,7 +144,7 @@ if __name__ == '__main__':
 		elif argv[1] == "default":#run the default file
 			executeMainFileToShell("src/main.pl")
 		else: #assume it is a datafile
-			worker(ntpath.basename(argv[1]))
+			worker(basename(argv[1]))
 
 	else:#read all the files in the data folder
 		jsonFiles = [f for f in listdir(dataFolder) if isfile(join(dataFolder, f)) and f[-5:] == ".json"]
@@ -115,4 +156,4 @@ if __name__ == '__main__':
 		outputResults(work_results)
 	print("-" * 50)
 
-	print("FINISHED")
+	print("FINISHED") '''
